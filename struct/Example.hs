2d6ccbd3a69e49c51c4c4a5a291a583af3cbe08d
simondlevy <simon.d.levy@gmail.com>
2024-02-06 02:42:17 -0500

    =More flight control

--- struct/Example.hs
+++ struct/Example.hs
@@ -30,7 +30,36 @@ data State = State {
   , dpsi   :: Float 
 }
 
+data Motors = QuadMotors { 
+                       qm1 :: Float
+                     , qm2 :: Float  
+                     , qm3 :: Float  
+                     , qm4 :: Float   
+               } |
 
+              HexMotors {
+                    hm1 :: Float  
+                  , hm2 :: Float  
+                  , hm3 :: Float  
+                  , hm4 :: Float  
+                  , hm5 :: Float  
+                  , hm6 :: Float  
+               } deriving (Show)
+
+
+type Mixer = Demands -> Motors
+
+quadXAPMixer :: Mixer
+quadXAPMixer demands = 
+    let t = (throttle demands)
+        r = (roll demands)
+        p = (pitch demands)
+        y = (yaw demands)
+    in QuadMotors (t - r - p - y)
+                  (t + r + p - y)
+                  (t + r - p + y)
+                  (t - r + p + y)
+ 
 ------------------------------------------------------------------------------
 ------------------------------------------------------------------------------
 
@@ -136,36 +165,7 @@ convertState_  (State_ (Field x)
 
 -------------------------------------------------------------------------------
 
-data Motors = QuadMotors { 
-                       qm1 :: Float
-                     , qm2 :: Float  
-                     , qm3 :: Float  
-                     , qm4 :: Float   
-               } |
 
-              HexMotors {
-                    hm1 :: Float  
-                  , hm2 :: Float  
-                  , hm3 :: Float  
-                  , hm4 :: Float  
-                  , hm5 :: Float  
-                  , hm6 :: Float  
-               } deriving (Show)
-
-
-type Mixer = Demands -> Motors
-
-quadXAPMixer :: Mixer
-quadXAPMixer demands = 
-    let t = (throttle demands)
-        r = (roll demands)
-        p = (pitch demands)
-        y = (yaw demands)
-    in QuadMotors (t - r - p - y)
-                  (t + r + p - y)
-                  (t + r - p + y)
-                  (t - r + p + y)
- 
 
 spec = do
 

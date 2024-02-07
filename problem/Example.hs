cda66254e24eea45668ff131964112f39bb26ffe
simondlevy <simon.d.levy@gmail.com>
2024-02-07 01:44:01 -0500

    =Cleanup

--- problem/Example.hs
+++ problem/Example.hs
@@ -5,7 +5,19 @@ import Copilot.Compile.C99
 
 import State
 
-import Problem
+fun3 :: Stream Float -> Stream Float -> Stream Float
+fun3 desired measured = desired - measured
+
+fun2 :: Stream Float -> Stream Float -> Stream Float
+fun2 desired measured = desired - measured
+
+fun1 :: State -> Stream Float
+
+fun1 state  = thrust'
+
+  where climbRate = fun3 0 (z state)
+
+        thrust' = fun2 climbRate (dz state)
 
 stateStruct :: Stream StateStruct
 stateStruct = extern "state" Nothing

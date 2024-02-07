f35338dbcc55510473bf29653802cdef42061388
simondlevy <simon.d.levy@gmail.com>
2024-02-07 01:53:55 -0500

    =Cleanup

--- problem/Example.hs
+++ problem/Example.hs
@@ -11,13 +11,6 @@ data StateStruct = StateStruct {
   , y'      :: Field "y " Float 
   , dy'     :: Field "dy" Float 
   , z'      :: Field "z" Float 
-  , dz'     :: Field "dz" Float 
-  , phi'    :: Field "phi" Float 
-  , dphi'   :: Field "dphi" Float 
-  , theta'  :: Field "theta" Float 
-  , dtheta' :: Field "dtheta" Float 
-  , psi'    :: Field "psi" Float 
-  , dpsi'   :: Field "dpsi" Float 
 }
 
 data State = State { 
@@ -26,13 +19,6 @@ data State = State {
   , y      :: Stream Float 
   , dy     :: Stream Float 
   , z      :: Stream Float 
-  , dz     :: Stream Float 
-  , phi    :: Stream Float 
-  , dphi   :: Stream Float 
-  , theta  :: Stream Float 
-  , dtheta :: Stream Float 
-  , psi    :: Stream Float 
-  , dpsi   :: Stream Float 
 }
 
 instance Struct StateStruct where
@@ -44,25 +30,11 @@ instance Struct StateStruct where
                  , Value Float (y' v)
                  , Value Float (dy' v)
                  , Value Float (z' v)
-                 , Value Float (dz' v)
-                 , Value Float (phi' v)
-                 , Value Float (dphi' v)
-                 , Value Float (theta' v)
-                 , Value Float (dtheta' v)
-                 , Value Float (psi' v)
-                 , Value Float (dpsi' v)
                  ]
 
 instance Typed StateStruct where
 
   typeOf = Struct (StateStruct
-                   (Field 0) 
-                   (Field 0) 
-                   (Field 0) 
-                   (Field 0)
-                   (Field 0)
-                   (Field 0)
-                   (Field 0)
                    (Field 0)
                    (Field 0)
                    (Field 0)
@@ -76,13 +48,7 @@ liftState state = State (state # x')
                         (state # y') 
                         (state # dy') 
                         (state # z') 
-                        (state # dz') 
-                        (state # phi') 
-                        (state # dphi') 
-                        (state # theta') 
-                        (state # dtheta') 
-                        (state # psi') 
-                        (state # dpsi') 
+
 fun3 :: Stream Float -> Stream Float -> Stream Float
 fun3 desired measured = desired - measured
 
@@ -91,11 +57,11 @@ fun2 desired measured = desired - measured
 
 fun1 :: State -> Stream Float
 
-fun1 state  = thrust'
+fun1 state  = o2
 
-  where climbRate = fun3 0 (z state)
+  where o3 = fun3 0 (z state)
 
-        thrust' = fun2 climbRate (dz state)
+        o2 = fun2 o3 (z state)
 
 stateStruct :: Stream StateStruct
 stateStruct = extern "state" Nothing
